7b1d4e3b9e33b9b01f0fa6feb269bb3a47db1dd3
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2023-08-09 14:51:58 +0200

    WIP

--- demo/models/berlin-metro-three-types.jl
+++ demo/models/berlin-metro-three-types.jl
@@ -4,7 +4,11 @@ include("./berlin-metro-adjacency.jl")
 
 degrees = vec(sum(A; dims=2))
 
-types = Dict(1 => findall(degrees .<= 2.0), 2 => findall(degrees .> 2.0))
+types = Dict(
+    1 => findall(degrees .<= 2.0),
+    2 => findall(2.0 .< degrees .<= 4.0),
+    3 => findall(degrees .> 4.0),
+)
 
 replace!(A, 0.0 => Inf) # ! Floy-Warshall
 A[diagind(A)] .= 0.0 # ! Floy-Warshall
