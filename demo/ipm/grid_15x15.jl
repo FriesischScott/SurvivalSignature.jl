d737b996437f56bf70698381c00a6f9d484d6e41
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2023-08-10 12:17:38 +0200

    Update grid 15x15 example

--- demo/ipm/grid_15x15.jl
+++ demo/ipm/grid_15x15.jl
@@ -5,8 +5,6 @@ using SurvivalSignature
 using Plots
 using Random
 
-@load "grid-network-15x15-10000.jld2"
-
 n = 15
 m = 15
 adj = gridnetwork(n, m)
@@ -20,35 +18,36 @@ wtol = 1e-3
 
 ci = [15, 15]
 
-# Φ, cov = survivalsignature(adj, types, φ)
-
-# jldsave("grid-network-$(n)x$(m)-$N.jld2"; signature, Φ, cov)
-
-signature_1 = IPMSurvivalSignature(
+signature_100 = IPMSurvivalSignature(
     adj, types, φ, ci; samples=10^2, covtol=covtol, wtol=wtol
 )
-signature_2 = IPMSurvivalSignature(
+jldsave("grid-network-$(n)x$(m)-IPM-100.jld2"; signature_100)
+
+signature_1000 = IPMSurvivalSignature(
     adj, types, φ, ci; samples=10^3, covtol=covtol, wtol=wtol
 )
-signature_3 = IPMSurvivalSignature(
+jldsave("grid-network-$(n)x$(m)-IPM-1000.jld2"; signature_1000)
+
+signature_10000 = IPMSurvivalSignature(
     adj, types, φ, ci; samples=10^4, covtol=covtol, wtol=wtol
 )
+jldsave("grid-network-$(n)x$(m)-IPM-10000.jld2"; signature_10000)
 
-distributions = Dict(1 => Exponential(1), 2 => Weibull(2, 1))
-t = collect(range(0.3, 0.5, 200))
+# distributions = Dict(1 => Exponential(1), 2 => Weibull(2, 1))
+# t = collect(range(0.3, 0.5, 200))
 
-Pl_1, Pu_1 = reliability(t, distributions, signature_1)
-Pl_2, Pu_2 = reliability(t, distributions, signature_2)
-Pl_3, Pu_3 = reliability(t, distributions, signature_3)
+# Pl_1, Pu_1 = reliability(t, distributions, signature_1)
+# Pl_2, Pu_2 = reliability(t, distributions, signature_2)
+# Pl_3, Pu_3 = reliability(t, distributions, signature_3)
 
-P = reliability(t, distributions, Φ)
-p = plot(t, P; lw=1.5, label="MC Approximation")
+# P = reliability(t, distributions, Φ)
+# p = plot(t, P; lw=1.5, label="MC Approximation")
 
-plot!(p, t, Pl_1; color="red", style=:dot, lw=1.5, label="IPM Envelopes 100")
-plot!(p, t, Pu_1; color="red", style=:dot, lw=1.5, label=:none)
+# plot!(p, t, Pl_1; color="red", style=:dot, lw=1.5, label="IPM Envelopes 100")
+# plot!(p, t, Pu_1; color="red", style=:dot, lw=1.5, label=:none)
 
-plot!(p, t, Pl_2; color="black", style=:dashdot, lw=1.5, label="IPM Envelopes 1000")
-plot!(p, t, Pu_2; color="black", style=:dashdot, lw=1.5, label=:none)
+# plot!(p, t, Pl_2; color="black", style=:dashdot, lw=1.5, label="IPM Envelopes 1000")
+# plot!(p, t, Pu_2; color="black", style=:dashdot, lw=1.5, label=:none)
 
-plot!(p, t, Pl_3; color="green", style=:dash, lw=1.5, label="IPM Envelopes 10000")
-plot!(p, t, Pu_3; color="green", style=:dash, lw=1.5, label=:none)
+# plot!(p, t, Pl_3; color="green", style=:dash, lw=1.5, label="IPM Envelopes 10000")
+# plot!(p, t, Pu_3; color="green", style=:dash, lw=1.5, label=:none)
